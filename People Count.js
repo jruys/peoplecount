445cf5ed610eaedb635ac285502665df029c2d4f
Jan-Willem Ruys <github@jruys.com>
2020-06-02 11:16:43 +0200

    Changed wording and moved max capacity to alert header

--- People Count.js
+++ People Count.js
@@ -3,17 +3,19 @@
 //
 // v0.1       Info: Count maximum people, show notification when >max nr of people in the room
 // v0.2       Changed text display method
+// v0.3       Moved maxPeople display to alert header
 
 const xapi = require('xapi');
-const maxPeople = 5
+const maxPeople = 1
 const alertDuration = 30
-console.log('MACRO STARTED - COVID-19 People Count');
+console.log('Debug: init - max = '+maxPeople);
 
 // display text on screen - can also be replaced by play-message, show-image, call-security etc
-function displayTextOnScreen(text) {
+function displayTextOnScreen(header,text) {
   xapi.command('UserInterface Message Alert Display', {
-  Title: 'COVID-19 ALERT',
-  Text: text, Duration: alertDuration,
+  Title: header,
+  Text: text, 
+  Duration: alertDuration,
   });
 }
 
@@ -22,8 +24,8 @@ function postStatusCall(amount) {
    console.log('DEBUG - Detected: ' + amount + ', max: ' + maxPeople);
    if (amount > maxPeople) {
        console.log('DEBUG - Alerting');
-       displayTextOnScreen("Too many people detected                       Room capacity = " + maxPeople)
-   };
+       displayTextOnScreen("Current room capacity: " + maxPeople, "To comply with social distancing guidelines this room's capacity is limited." )
+   }
 }
 
 // Get dynamic PEOPLE COUNT STATUS updates
